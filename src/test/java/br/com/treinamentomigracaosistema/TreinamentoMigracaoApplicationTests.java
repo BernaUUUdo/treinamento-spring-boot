2f32bf10375a167414db25ce731b90698d7eded9
Bernard Correa Pereira <bernard.pereira@dataprev.gov.br>
2020-03-20 16:40:33 -0300

    criando projeto

---
+++ src/test/java/br/com/treinamentomigracaosistema/TreinamentoMigracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamentomigracaosistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoMigracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
